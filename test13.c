13fe413579170d06a0cf0117882a890e5e1f0c6d
Hannah Gaven <hannah.m.gaven.th@dartmouth.edu>
2021-10-07 14:21:36 -0400

    thurs end of office hours

--- test13.c
+++ test13.c
@@ -1,7 +1,6 @@
-/* test11.c --- testing qget implicity test qput
- * --> should pass as there are things to get
- * --> tests qput both for putting something on an empty list
- * and for putting somethingona list with other things
+/* test13.c --- testing qget
+ * Should fail because nothing is in the queue and we try to get something
+ * 
  * 
  * Author: Ezgi Okutan
  * Created: Thu Sep 30 21:34:08 2021 (-0400)
@@ -45,11 +44,10 @@ car_t *front=NULL;
 
 car_t *make_car(char *plate,double price,int year)  { 
 	car_t *pp;
-	pp = (car_t*)malloc(sizeof(car_t));
-	//if(!(pp = (car_t*)malloc(sizeof(car_t)))) {
-		//printf("[Error: malloc failed allocating car]\n");
-		//return NULL;
-	//}
+	if(!(pp = (car_t*)malloc(sizeof(car_t)))) {
+		printf("[Error: malloc failed allocating car]\n");
+		return NULL;
+	}
 	pp->next = NULL;
 	strcpy(pp->plate,plate);
 	pp->price=price;
@@ -59,32 +57,14 @@ car_t *make_car(char *plate,double price,int year)  {
 
 int main(void){
   	car_t *queuep= qopen();
-	car_t *p1 = make_car("1",3000,2005);
-	car_t *p2 = make_car("2",3002,2006);
-	car_t *p3 = make_car("3",3003,2007);
-
-  	qput(queuep,(void *)p1);
-	qput(queuep,(void *)p2);
-	qput(queuep,(void *)p3);
-	qapply(queuep, printcar);
 	printf("....\n");
-	void *carp1= qget(queuep);
-	car_t* cp1 = (car_t *)carp1;
-	//void *carp2= qget(queuep);
-	//car_t* cp2 = (car_t *)carp2;
-	//void *carp3= qget(queuep);
-	//car_t* cp3 = (car_t *)carp3;
-	if(cp1!=NULL){
-		qclose(queuep);
-		//free(p1);
-		//free(p2);
-		//free(p3);
+	void *carp= qget(queuep);
+	car_t* cp = (car_t *)carp;
+	if(cp!=NULL){
+		qapply(queuep, printcar);
 		exit(EXIT_SUCCESS);
+	}else{
+		exit(EXIT_FAILURE);
 	}
-	qclose(queuep);
-	//free(p1);
-	//free(p2);
-	//free(p3);
-	exit(EXIT_FAILURE);
-
+	exit(EXIT_SUCCESS);
 }
